42b858661360b3f3746c2fe02b38bcd4daf1f194
Laura Purcell-Gates <l.purcellgates@gmail.com>
2023-08-07 16:03:08 +0100

    added header

--- CS50-a-sql-python-prophecy/prophecy.py
+++ CS50-a-sql-python-prophecy/prophecy.py
@@ -1,3 +1,22 @@
+"""
+    PROGRAM :  Hall of Prophecy
+    AUTHOR  :  Laura Purcell-Gates
+    EMAIL   :  <laurapurcellgates@gmail.com>
+
+    CS50x 2023 Practice Problem 7 Hall of Prophecy
+
+    I refactored a SQL database to avoid redundancies, and 
+    wrote a Python program to load new SQL tables linking 
+    students to house assignments. 
+
+    My refactored database and schema are roster.db and schema.sql.
+    Original database and schema roster-orig.db and schema-orig.sql.
+
+    Code fully written by me; no pre-filled CS50 code.
+
+    Link to the practice problem:
+    https://cs50.harvard.edu/x/2023/problems/7/prophecy/  """
+
 import csv
 from cs50 import SQL
 
